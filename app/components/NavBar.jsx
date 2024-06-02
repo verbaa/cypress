62b0ed81f56774ed47745eb2013fa0889910b705
verbaa <verba.dp@gmail.com>
2024-06-02 11:36:32 +0300

    Initial commit

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import NavItem from "./NavItem"
+
+const navItems = [
+    {
+    label: 'Fundamentals',
+    path: '/fundamentals',
+    dataTest: 'nav-fundamentals'
+    },
+    {
+        label: 'Forms',
+        path: '/forms',
+        dataTest: 'nav-forms'
+    },
+    {
+        label: 'Examples',
+        path: '/examples',
+        dataTest: 'nav-examples'
+    },
+    {
+        label: 'Component',
+        path: '/component',
+        dataTest: 'nav-component' 
+    },
+    {
+        label: 'Best Practices',
+        path: '/best-practices',
+        dataTest: 'nav-best-practices'
+    },
+]
+
+export default function NavBar(){
+    return (
+        <ul className="nav-bar">
+            {
+                navItems.map((item)=> (
+                    <NavItem 
+                    key={item.label} 
+                    label={item.label} 
+                    path={item.path} 
+                    dataTest={item.dataTest}
+                    />
+                ))
+            }
+        </ul>
+    )
+}
